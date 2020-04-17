3eb101c5059d2cc267229b211fe3b6b459db4fbd
WoodhamsBD <woodhamsbd@gmail.com>
2020-04-16 17:18:49 -0700

    Items

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// Prisma-Binding, allows for JS use in prisma
+// conmnects to remote prisma DB
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+// Start it all in here
+require('dotenv').config({ path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Express Middleware happens here
+
+
+// Start 
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  },
+}, details => {
+  console.log(`Server is now running on port http://localhost:${details.port}`)
+});
