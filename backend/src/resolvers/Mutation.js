9eac6afa3593c2178e2319ffcca0367e16ecfd97
WoodhamsBD <woodhamsbd@gmail.com>
2020-04-24 14:12:02 -0700

    Prisma + Yoga setup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parents, args, ctx, info) {
+  // TODO Check if logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    return item;
+  }
+};
 
 module.exports = Mutations;
