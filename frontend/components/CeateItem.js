bcb75af0c10cade2c1e8987150240ed06ccdd7d6
WoodhamsBD <woodhamsbd@gmail.com>
2020-04-29 15:11:18 -0700

    calling it

--- frontend/components/CeateItem.js
+++ frontend/components/CeateItem.js
@@ -6,6 +6,7 @@ import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
+import { FieldsOnCorrectType } from 'graphql/validation/rules/FieldsOnCorrectType';
 
 
 const CREATE_ITEM_MUTATION = gql`
@@ -45,6 +46,27 @@ class CeateItem extends Component {
     this.setState({ [name]: val}); // matches name to corresponding state and sets to value of target, computed property names
   };
 
+
+  // Uploading of Images to Cloudinary
+  uploadFile = async e => {
+    console.log("Inside File upload");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits2');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/ddzewcsfg/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  };
+
   render() {
     return (
     // Wrap whole form in the mutation created above to expose it to the form
@@ -62,6 +84,11 @@ class CeateItem extends Component {
         }}>
         <Error error={error}/>
         <fieldset disabled={loading} aria-busy={loading}>
+          <label htmlFor="file">
+            Image
+          <input type="file" id="file" name="file" placeholder="Upload an Image" required onChange={this.uploadFile}/>
+          {this.state.image && <img src={this.state.image} alt="Upload Preview"/>}
+          </label>
           <label htmlFor="title">
             Title
             <input type="title" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange}/>
